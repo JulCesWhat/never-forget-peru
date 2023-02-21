20b37d2d5f1bfcc8f4f1d5f1b6b94878df5dc217
Cesar Whatley <julceswhat@gmail.com>
2023-02-20 22:21:49 -0600

    Completing contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,75 @@
+import React, { useState } from "react";
+
+import "./Contact.scss"
+
+const emailEndpoint = "http://localhost:5000/contact";
+
 function Contact() {
+    const [submitting, setSubmitting] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        
+        const { name, email, message } = e.target.elements;
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+
+        try {
+            let response = await fetch(emailEndpoint, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json;charset=utf-8",
+                },
+                body: JSON.stringify(details),
+            });
+
+            await response.json();
+            setSubmitted(true)
+        } catch (error) {
+            alert(error);
+            setSubmitted(false);
+        } finally {
+            setSubmitting(false)
+        }
+    };
+
     return (
-        <div>Contact Page!!</div>
-    )
+        <section>
+            <div className="form-container">
+                <div className="contact-form-item">
+                    {
+                        submitted ? (
+                            <div className="thanks-result">
+                                <div>Muchas gracias!</div>
+                                <div>Estaremos en contacto pronto.</div>
+                            </div>
+                        ) : (
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <label htmlFor="name">Nombre:</label>
+                                    <input type="text" id="name" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="email">Email:</label>
+                                    <input type="email" id="email" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="message">Mensaje:</label>
+                                    <textarea id="message" required />
+                                </div>
+                                <button type="submit" disabled={submitting}>Enviar</button>
+                            </form>
+                        )
+                    }
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Contact;
