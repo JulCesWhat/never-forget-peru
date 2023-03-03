400b02f061cbb2729056c6497775d75e650d00af
Cesar Whatley <julceswhat@gmail.com>
2023-03-03 15:40:00 -0600

    Adding person details page

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,13 +1,18 @@
 import { useParams } from 'react-router-dom'
 
-import "./Persons.scss"
+import "./Person.scss"
 
-function Person({ individuals }) {
-    const { userId } = useParams();
+function Person({ individual }) {
     return (
-        <section className="persons-container">
-            <div className="container flex-grid">
-                The User {userId}
+        <section className="person-container">
+            <div className="container">
+                <div class="person-card">
+                    <div class="person-img-header">
+                        <h2>Card Title</h2>
+                    </div>
+                    <div class="date">30 JULY 2016</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+                </div>
             </div>
         </section>
     );
